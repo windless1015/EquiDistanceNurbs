0ac99aed26f3760e33e070514d0a7524a7f00968
windless1015 <shangtao1989@qq.com>
2020-02-10 22:34:30 +0800

    目前可以找到自交的点

--- EquiDistanceNurbs/EquiDistanceNurbs.cpp
+++ EquiDistanceNurbs/EquiDistanceNurbs.cpp
@@ -14,16 +14,28 @@ EquiDistanceNurbs::EquiDistanceNurbs(QWidget *parent)
 	isShowCtrlPtsConnectedLine = false;
 	isShowCurvatureRadius = false;
 	isShowBodyPtsNorm = false;
-
+	isShowRawEquiDistance = false;
 	//lineedit只能输入数字
 	ui.lineEdit_idx->setValidator(new QIntValidator(0, 10000, this));
 
 	connect(ui.btnDisplayTan, &QPushButton::clicked, this, &EquiDistanceNurbs::OnBtnDisplayTangent);
 	connect(ui.btnDisplayNorm, &QPushButton::clicked, this, &EquiDistanceNurbs::OnBtnDisplayNorm);
-	connect(ui.btnOffsetCurve, &QPushButton::clicked, this, &EquiDistanceNurbs::OnBtnEquidistance);
-	connect(ui.btnOffsetCurve, &QPushButton::clicked, this, &EquiDistanceNurbs::OnBtnEquidistance);
+	connect(ui.btnShowOriginEquiLines, &QPushButton::clicked, this, &EquiDistanceNurbs::OnBtnRawEquidistance);
 	connect(ui.btnDisplayCurvatureRad, &QPushButton::clicked, this, &EquiDistanceNurbs::OnBtnDisplayCurvature);
 	connect(ui.lineEdit_idx, &QLineEdit::returnPressed, this, &EquiDistanceNurbs::OnDisplayCurvature);
+
+	NurbsCtrlPoint p1;
+	p1.point = QPointF(243, 676);
+	p1.weight = 1.0;
+	nurbsCtrlPoints.push_back(p1);
+	p1.point = QPointF(244, 304);
+	nurbsCtrlPoints.push_back(p1);
+	p1.point = QPointF(356, 672);
+	nurbsCtrlPoints.push_back(p1);
+	p1.point = QPointF(446, 290);
+	nurbsCtrlPoints.push_back(p1);
+	p1.point = QPointF(450, 695);
+	nurbsCtrlPoints.push_back(p1);
 }
 
 void EquiDistanceNurbs::OnBtnDisplayCurvature()
@@ -52,9 +64,13 @@ void EquiDistanceNurbs::OnBtnDisplayNorm()
 	update();
 }
 
-void EquiDistanceNurbs::OnBtnEquidistance()
+void EquiDistanceNurbs::OnBtnRawEquidistance()
 {
-	
+
+	isShowRawEquiDistance = !isShowRawEquiDistance;
+	CalOffsetCurve(originNurbsBodyPts, 40);
+
+	update();
 }
 
 void EquiDistanceNurbs::mousePressEvent(QMouseEvent* event)
@@ -64,7 +80,7 @@ void EquiDistanceNurbs::mousePressEvent(QMouseEvent* event)
 		NurbsCtrlPoint oneCtrlPt;
 		oneCtrlPt.point = event->pos();;
 		oneCtrlPt.weight = 1.0;
-		nurbsCtrlPoints.push_back(oneCtrlPt);
+		//nurbsCtrlPoints.push_back(oneCtrlPt);
 		GenerateNURBSCurve();
 		CalFirstDerivativeAndNorm(originNurbsBodyPts);
 		CalSecondDerivativeAndCurvRad(originNurbsBodyPts);
@@ -138,14 +154,12 @@ void EquiDistanceNurbs::paintEvent(QPaintEvent *event)
 	{
 		QPen tanPen(QColor(1, 0, 0));
 		tanPen.setWidth(1);
-		//painter.setPen(tanPen);
-
-
 		painter.setPen(QColor(Qt::blue));
 		painter.setBrush(QBrush(Qt::white));
+		QPointF unitTangent;
 		for (int i = 0; i < originNurbsBodyPts.size(); i++)
 		{
-			QPointF unitTangent = originNurbsBodyPts[i].firstDerivative;
+			unitTangent = originNurbsBodyPts[i].firstDerivative;
 			unitTangent /= sqrt(unitTangent.x() * unitTangent.x() + unitTangent.y() * unitTangent.y());
 			QPointF tangentPt = 80 * unitTangent + originNurbsBodyPts[i].point;
 			//绘制每一小段的直线,当body pts足够多的时候就是曲线
@@ -167,6 +181,44 @@ void EquiDistanceNurbs::paintEvent(QPaintEvent *event)
 		painter.drawEllipse(circleCenter, r, r);
 	}
 
+	//根据原始的nurb曲线显示raw等距线(有自交,未经修饰过的)
+	if (isShowRawEquiDistance)
+	{
+		QPen rawEquiPen(QColor(1, 0, 0));
+		rawEquiPen.setWidth(1);
+		painter.setPen(QColor(Qt::blue));
+		QPointF rawEquiNurbPt_prev;
+		QPointF rawEquiNurbPt_follw;
+		int vecSize = originNurbsBodyPts.size();
+		for (int i = 0; i < vecSize - 2; i++)
+		{
+			rawEquiNurbPt_prev = originNurbsBodyPts[i].unitNormVector * 40 + originNurbsBodyPts[i].point;
+			rawEquiNurbPt_follw = originNurbsBodyPts[i+1].unitNormVector * 40 + originNurbsBodyPts[i+1].point;
+			//绘制每一小段的直线,当body pts足够多的时候就是曲线
+			painter.drawLine(rawEquiNurbPt_prev, rawEquiNurbPt_follw);
+		}
+		//再绘制最后一段
+		rawEquiNurbPt_prev = rawEquiNurbPt_follw;
+		rawEquiNurbPt_follw = originNurbsBodyPts[vecSize - 1].unitNormVector * 40 + originNurbsBodyPts[vecSize - 1].point;
+		painter.drawLine(rawEquiNurbPt_prev, rawEquiNurbPt_follw);
+	
+	}
+
+	//临时显示
+	{
+		QPen ctrlPtsPen(QColor(0, 0, 255));
+		ctrlPtsPen.setWidth(5);
+		painter.setPen(ctrlPtsPen);
+		QBrush brush(Qt::SolidPattern);//画刷
+		brush.setColor(Qt::blue);
+		painter.setBrush(brush);//设置画刷
+		for (int i = 0; i < locationVec.size(); i++)
+		{
+			QPointF tmpPt = locationVec[i].point;
+			tmpPt = tmpPt + locationVec[i].unitNormVector * 40;
+			painter.drawPoint(tmpPt.x(), tmpPt.y());
+		}
+	}
 }
 
 double EquiDistanceNurbs::N3Spline(int i, double u)
@@ -269,4 +321,100 @@ void EquiDistanceNurbs::CalSecondDerivativeAndCurvRad(QVector<NurbsBodyPoint>&bo
 	bodyPtsVector[0].curvatureRadius = bodyPtsVector[1].curvatureRadius;
 	bodyPtsVector[vectorSize - 1].secondDerivative = bodyPtsVector[vectorSize - 2].secondDerivative;
 	bodyPtsVector[vectorSize - 1].curvatureRadius = bodyPtsVector[vectorSize - 2].curvatureRadius;
+}
+
+///////////////////////////////////算法函数///////////////////////////
+void EquiDistanceNurbs::FindPtsCurvRadLowerThanOffset(const QVector<NurbsBodyPoint>&nurbsBodyPts,
+	const int& offset, QVector<int>& ptsCurvRadLower)
+{
+	int nurbsBodyPtSize = nurbsBodyPts.size();
+	if (nurbsBodyPtSize <= 0)
+		return;
+	int j = 0;
+	double curvRad;
+	while (j < nurbsBodyPtSize)
+	{
+		while (nurbsBodyPts[j].curvatureRadius > qAbs(offset) && j <= nurbsBodyPtSize - 2) //遍历到导数第2个点
+		{
+			j++;
+		}
+		if (j == nurbsBodyPtSize - 1) //遍历到最后一个点就跳出
+		{
+			break;
+		}
+		ptsCurvRadLower.push_back(j);//起点
+		while (nurbsBodyPts[j].curvatureRadius <= qAbs(offset) && j < nurbsBodyPtSize)
+		{
+			j++;
+		}
+		ptsCurvRadLower.push_back(j - 1); //回退一个索引, 终点
+	}
+
+}
+
+void EquiDistanceNurbs::CalOffsetCurve(const QVector<NurbsBodyPoint>&bodyPtsVector, int offset)
+{
+	//1.获取nurbs曲线上小于曲率半径的首末端点
+    QVector<int> startEndPtLowerCurvRad;       
+	FindPtsCurvRadLowerThanOffset(bodyPtsVector, offset, startEndPtLowerCurvRad);
+
+	//2.将startEndPtLowerCurvRad存放的曲率半径小于offset的点, 剔除相同点放入purifyEndPts
+	QVector<int> purifyEndPts;
+	if (startEndPtLowerCurvRad.size() > 0)
+	{
+		int comparedIdx = startEndPtLowerCurvRad.at(0); //获取第一个待比较的idx
+		purifyEndPts.push_back(comparedIdx); //第一个数据肯定存在,压入
+		for (int i = 1; i < startEndPtLowerCurvRad.size(); i++)
+		{
+			int curIdx = startEndPtLowerCurvRad[i];
+			if (comparedIdx != curIdx)
+			{
+				purifyEndPts.push_back(curIdx);
+				comparedIdx = curIdx;
+			}
+		}
+	}
+	//3. 将purifyEndPts中不产生自交的点删除,剩下的是自交的点
+	double con;
+	QVector<int> needCutEndPts;  //需要裁剪的首末端点
+	for (int i = 0; i < purifyEndPts.size(); i++)
+	{
+		con = CalConvexHull(bodyPtsVector, purifyEndPts.at(i));
+		if (offset > 0 && con > 0)
+		{
+			needCutEndPts.push_back(purifyEndPts.at(i));
+		}
+		else if (offset < 0 && con < 0)
+		{
+			needCutEndPts.push_back(purifyEndPts.at(i));
+		}
+	}
+
+
+	//显示其偏移点
+	for (int i = 0; i < needCutEndPts.size(); i++)
+	{
+		locationVec.push_back(bodyPtsVector.at(needCutEndPts.at(i)));
+	}
+
+}
+
+
+double EquiDistanceNurbs::CalConvexHull(const QVector<NurbsBodyPoint>&bodyPtsVector, const int& index)
+{
+	if (index < 1)
+		return 0;
+	QPointF p0, p1, p2;
+	double result = 0;
+	p0 = bodyPtsVector.at(index - 1).point;
+	p1 = bodyPtsVector.at(index).point;
+	p2 = bodyPtsVector.at(index + 1).point;
+
+	//本质就是利用向量叉乘 来判断是否是凸包还是凹的
+	double denom1, denom2;
+	denom1 = p2.x() - p1.x();
+	denom2 = p1.y() - p0.y();
+	if ((fabs(denom1) > 1e-5) && (fabs(denom2) > 1e-5))
+		result = (p1.x() - p0.x())*(p2.y() - p1.y()) - denom1 * denom2;  //论文 式6的行列式,判断正负用
+	return result;
 }
--- EquiDistanceNurbs/EquiDistanceNurbs.h
+++ EquiDistanceNurbs/EquiDistanceNurbs.h
@@ -34,7 +34,7 @@ public:
 
 
 public slots:
-	void OnBtnEquidistance();
+	void OnBtnRawEquidistance();
 	void OnBtnDisplayTangent();
 	void OnBtnDisplayNorm();
 	void OnBtnDisplayCurvature();
@@ -50,12 +50,12 @@ private:
 	//三阶 样条
 	const int DEGREE = 3;
 	//点之间的步长距离
-	const double step = 0.1;
-
+	const double step = 0.01;
 	//原始nurbs曲线的控制点
 	QVector<NurbsCtrlPoint>			nurbsCtrlPoints;
 	//原始nurbs曲线上的点
 	QVector<NurbsBodyPoint>		originNurbsBodyPts;
+	QVector<NurbsBodyPoint> locationVec;
 
 private:
 	bool isShowCtrlPoints; //是否显示控制点
@@ -63,6 +63,7 @@ private:
 	bool isShowBodyPtsTangent; //是否显示曲线点的切向量
 	bool isShowBodyPtsNorm; //是否显示曲线点的法向量
 	bool isShowCurvatureRadius; //是否显示某一个点的曲率半径
+	bool isShowRawEquiDistance; //是否显示原始等距线
 
 private:
 	void GenerateNURBSCurve();
@@ -71,4 +72,14 @@ private:
 	void CalFirstDerivativeAndNorm(QVector<NurbsBodyPoint>&);
 	//计算二阶导数和曲率半径
 	void CalSecondDerivativeAndCurvRad(QVector<NurbsBodyPoint>&);
+	//计算偏移曲线
+	void CalOffsetCurve(const QVector<NurbsBodyPoint>&, int);
+
+	//算法函数
+private: 
+	//寻找原始nurbs曲线上曲率半径小于给定offset值的点,(如果首位点相同,则记录两次,当作距离为0的线段)
+	//所谓ptsCurvRadLower得到的个数一定是偶数
+	void FindPtsCurvRadLowerThanOffset(const QVector<NurbsBodyPoint>&, const int&, QVector<int>&);
+	//计算凸包
+	double CalConvexHull(const QVector<NurbsBodyPoint>&, const int&);
 };
